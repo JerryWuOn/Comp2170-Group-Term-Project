c8ddec8b627c88267960ed53a7459e405994c999
TeoEstrada9 <mateo.feriaestrada@gmail.com>
2023-05-08 12:48:33 -0700

    category section changes

--- category.js
+++ category.js
@@ -3,3 +3,14 @@ fetch("https://opentdb.com/api_category.php")
 .then(data => console.log(data))
 .catch(error => console.log(error))
 
+fetch("https://opentdb.com/api.php?amount=10&category=9")
+.then(response => response.json())
+.then(questions => {
+    const questionsContainer = document.querySelector('#general_knowledge .questions');
+    questions.forEach(question => {
+      const questionElement = document.createElement('div');
+      questionElement.textContent = question.text;
+      questionsContainer.appendChild(questionElement);
+    });
+  });
+
