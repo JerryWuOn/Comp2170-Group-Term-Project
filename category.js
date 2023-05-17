9e9de75d2524b18cfaeeb65350448db9ef26b6f4
TeoEstrada9 <mateo.feriaestrada@gmail.com>
2023-05-17 11:50:15 -0700

    adding new changes for fetch statement

--- category.js
+++ category.js
@@ -1,48 +1,37 @@
 import { categoryImages } from "./config.js";
-// Variables & DOM Elements
 
 const categoryItems = document.querySelector(".categories");
 let dataCategories;
 
-// Functions
-
 function questionsLoad(dataCategories) {
   dataCategories.forEach((category) => {
-    let getID = category.id;
-    let getName = category.name;
+    const { id, name } = category;
 
-    let categoryElement = document.createElement("section");
-    categoryElement.innerHTML = `<img class="category_picture" src="images/${categoryImages[getID]}"/> `;
-    let newElement = document.createElement("div");
-    newElement.className = "banner";
-    let NewLinkTag = document.createElement("a");
-    NewLinkTag.setAttribute("href", "question.html");
-    NewLinkTag.innerHTML = getName
-    categoryElement.appendChild(newElement);
-    newElement.append(NewLinkTag);
+    const categoryElement = document.createElement("section");
+
+    const imageElement = document.createElement("img");
+    imageElement.classList.add("category_picture");
+    imageElement.src = `images/${categoryImages[id]}`;
+
+    const bannerElement = document.createElement("div");
+    bannerElement.classList.add("banner");
+
+    const linkElement = document.createElement("a");
+    linkElement.classList.add("title");
+    linkElement.href = "question.html";
+    linkElement.textContent = name;
+
+    bannerElement.appendChild(linkElement);
+    categoryElement.appendChild(imageElement);
+    categoryElement.appendChild(bannerElement);
     categoryItems.appendChild(categoryElement);
-
-    //   <section class="categories_section">
-    //   <img class="category_picture" src="images/anime.jpg" />
-    //   <div class="banner">
-    //     <a class="anime-banner" class="title" href="question.html"
-    //       >Japanese Anime & Manga</a
-    //     >
-    //   </div>
-    // </section>
   });
 }
 
-// Event Listeners
-
 window.onload = () => {
-  // fetch our data
   fetch("https://opentdb.com/api_category.php")
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-      // {trivia_categories}
-      data.trivia_categories;
       dataCategories = data.trivia_categories;
       questionsLoad(dataCategories);
     })
--- questions.js
+++ questions.js
@@ -1,42 +1,78 @@
 const startingMinutes = 5;
-let time = startingMinutes * 60; 
+let time = startingMinutes * 60;
 let alertDisplayed = false;
 
-const countdown = document.getElementById('countdown')
+const countdown = document.getElementById('countdown');
+const messageContainer = document.getElementById('messageContainer');
 
-const intervalId = setInterval(updateCountdown, 1000)
+const intervalId = setInterval(updateCountdown, 1000);
 
 function updateCountdown() {
-    if(time <= -1 && !alertDisplayed) {
-        alert('You have run out of time please try again')
-        alertDisplayed = true;
-        clearInterval(intervalId)
-        return; 
-    } 
+  if (time <= -1 && !alertDisplayed) {
+    alertDisplayed = true;
+    clearInterval(intervalId);
+    alert('Time has ended. Redirecting to the main screen...');
+    setTimeout(() => {
+      window.location.href = 'index.html'; // Replace 'index.html' with the actual filename of your main screen
+    }, 1000); // Adjust the delay (in milliseconds) before redirecting to the main screen
+    return;
+  }
 
-    const minutes = Math.floor(time/60)
-    let seconds = time % 60; 
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
 
+  seconds = seconds < 10 ? '0' + seconds : seconds;
 
-    seconds = seconds < 10 ? '0' + seconds : seconds
-
-    countdown.innerHTML = `${minutes}: ${seconds}`
-    time--;
+  countdown.innerHTML = `${minutes}:${seconds}`;
+  time--;
 }
 
-fetch("https://opentdb.com/api.php?amount=10&category=9")
+function showMessage(message) {
+  const messageElement = document.createElement('p');
+  messageElement.textContent = message;
+  messageContainer.appendChild(messageElement);
+}
+
+
+
+fetch("https://opentdb.com/api.php?amount=10&type=multiple")
 .then(response => response.json())
-.then(questions => {
+.then(responseData => {
     const questionsContainer = document.querySelector('#questionsContainer');
-    questions.forEach(question => {
+    responseData.results.forEach(question => {
       const questionElement = document.createElement('div');
-      questionElement.textContent = question.question;
+      questionElement.innerHTML = question.question;
       questionsContainer.appendChild(questionElement);
+
+      const answersElement = document.createElement('ul');
+      question.incorrect_answers.forEach(incorrectAnswer => {
+        const incorrectAnswerElement = document.createElement('li');
+        incorrectAnswerElement.innerHTML = incorrectAnswer;
+        incorrectAnswerElement.classList.add('questions')
+        answersElement.appendChild(incorrectAnswerElement);
+        incorrectAnswerElement.addEventListener('click', () => {
+          incorrectAnswerElement.classList.add('selected')
+        })
+      });
+      
+      const correctAnswerElement = document.createElement('li');
+      correctAnswerElement.innerHTML = question.correct_answer;
+      correctAnswerElement.classList.add('questions')
+      answersElement.appendChild(correctAnswerElement);
+      correctAnswerElement.addEventListener('click', () => {
+        correctAnswerElement.classList.add('selected')
+      })
+
+      questionElement.appendChild(answersElement);
+      questionsContainer.appendChild(questionElement);
+      console.log(question)
+     
     });
   })
   .catch(error => {
     console.log(error);
   });
+
 
 
   
