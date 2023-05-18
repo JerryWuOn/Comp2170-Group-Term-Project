32c906edf727224f279968333a3497cecc3f9970
TeoEstrada9 <mateo.feriaestrada@gmail.com>
2023-05-18 10:49:34 -0700

    new change

--- config.js
+++ config.js
@@ -25,4 +25,50 @@ const categoryImages = {
   "32": "cartoons.jpg",
 };
 
-export {categoryImages};
+const categoryItems = document.querySelector(".categories");
+let dataCategories;
+
+function questionsLoad(dataCategories) {
+  dataCategories.forEach((category) => {
+    const { id, name } = category;
+
+    const categoryElement = document.createElement("section");
+
+    const imageElement = document.createElement("img");
+    imageElement.classList.add("category_picture");
+    imageElement.src = `images/${categoryImages[id]}`;
+
+    const bannerElement = document.createElement("div");
+    bannerElement.classList.add("banner");
+
+    const linkElement = document.createElement("a");
+    linkElement.classList.add("title");
+    linkElement.href = "question.html";
+    linkElement.textContent = name;
+
+    bannerElement.appendChild(linkElement);
+    categoryElement.appendChild(imageElement);
+    categoryElement.appendChild(bannerElement);
+    categoryItems.appendChild(categoryElement);
+
+    // Add a click event listener to the link element
+    linkElement.addEventListener("click", () => {
+      // Store the selected category ID and corresponding image URL in localStorage
+      localStorage.setItem("selectedCategoryId", id);
+      localStorage.setItem("selectedCategoryImage", categoryImages[id]);
+    });
+  });
+}
+
+window.onload = () => {
+  fetch("https://opentdb.com/api_category.php")
+    .then((response) => response.json())
+    .then((data) => {
+      dataCategories = data.trivia_categories;
+      questionsLoad(dataCategories);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+export{categoryImages}
